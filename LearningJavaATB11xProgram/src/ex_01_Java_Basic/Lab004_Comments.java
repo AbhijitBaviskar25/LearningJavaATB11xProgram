f6186f4a59dcc18db076e2cc7a4d649384b22728
ADMIN <Abhijitbaviskar25@yahoo,in>
2025-08-10 11:32:07 +0530

    comments

---
+++ LearningJavaATB11xProgram/src/ex_01_Java_Basic/Lab004_Comments.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments {
+	//single line comment
+	// This is a single line comment
+   /* this is a multi line comment
+    name: abhijit
+    course: ATB11X
+    java program
+     */
+    public static void main(String[] args) {
+        System.out.println("Comment program");
+
+  }
+}
