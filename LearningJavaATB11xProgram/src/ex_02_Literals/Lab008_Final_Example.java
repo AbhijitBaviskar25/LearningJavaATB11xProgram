41fa0f8e3228790fb846d3ca882516ac8b503da0
ADMIN <Abhijitbaviskar25@yahoo,in>
2025-08-10 11:47:22 +0530

    final

---
+++ LearningJavaATB11xProgram/src/ex_02_Literals/Lab008_Final_Example.java
@@ -0,0 +1,14 @@
+package ex_02_Literals;
+
+public class Lab008_Final_Example {
+	public static void main(String[] args) {
+		int local;
+        local = 77;
+        System.out.println(local);
+
+        final float PI = 3.14f;
+        // PI = 3.13f; This is final in nature.
+        System.out.println(PI);
+	}
+
+}
