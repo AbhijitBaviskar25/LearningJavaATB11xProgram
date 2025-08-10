275353951d2db7594046f9c9636a483cc1028d2f
ADMIN <Abhijitbaviskar25@yahoo,in>
2025-08-10 13:16:15 +0530

    Operators

---
+++ LearningJavaATB11xProgram/src/ex_03_Operators/Lab016_Relational_Operator.java
@@ -0,0 +1,27 @@
+package ex_03_Operators;
+
+public class Lab016_Relational_Operator {
+	 public static void main(String[] args) {
+	        // < Less Than
+	        // < =  -> Less than or equal to
+	        // > Greater
+	        // > = Greater or equal
+	        // == ->  Equal to (but checking)
+	        // != -> Not equal
+
+	        // All of them will result boolean output.
+
+	        int a = 10;
+	        int b = 30;
+	        boolean c = a > b; // // 10> 30
+	        System.out.println(c);
+
+	        int age_surya = 34;
+	        int age_Abhi = 33;
+	        boolean result = age_surya>= age_Abhi;
+	        System.out.println(result);
+
+
+	        // age_Abhi > age_surya or age_Abhi = age_surya
+	    }
+}
