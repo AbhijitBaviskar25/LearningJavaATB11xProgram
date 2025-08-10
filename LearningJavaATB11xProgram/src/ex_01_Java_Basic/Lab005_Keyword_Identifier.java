42a961b478244534de90e7ee93ebdfbe1ca1031c
ADMIN <Abhijitbaviskar25@yahoo,in>
2025-08-10 11:37:21 +0530

    Arithmatic operation

---
+++ LearningJavaATB11xProgram/src/ex_01_Java_Basic/Lab005_Keyword_Identifier.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab005_Keyword_Identifier {
+	//package -> It's keyword
+	// JavaConcept.JavaConcept.ex_01_Java_Basic - Identifier
+	    //public -> Keyword
+	    //class -> keyword
+	    //Lab005_Keyword_Identifiers - Identifier
+	    public static void main(String[] args) {
+	        //public - keyword
+//	        staic-> keyword
+//	        void -> keyword
+	        System.out.println("Keyword and Identifiers");
+
+	    }
+}
