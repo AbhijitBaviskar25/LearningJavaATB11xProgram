e5b8f248db64e5f7f977b393709ca4a12d231852
ADMIN <Abhijitbaviskar25@yahoo,in>
2025-08-12 15:25:46 +0530

    ternary

---
+++ LearningJavaATB11xProgram/src/ex_05_Ternary_Operator/Lab029_VoterAg_TO.java
@@ -0,0 +1,18 @@
+package ex_05_Ternary_Operator;
+
+import java.util.Scanner;
+
+public class Lab029_VoterAg_TO {
+	 public static void main(String[] args) {
+		  int age = 10;
+	        String canIVote  =  10 >=18 ? "Yes, You can Vote" : "No You can't Vote";
+	        System.out.println(canIVote);
+	    }
+//	 public static void main(String[] args) {
+//	        String age1 = args[0];
+//	       Scanner age2=new Scanner(System.in);
+//	        int age = Integer.parseInt(age1);
+//	        String canIVote = age >= 18 ? "Yes you can vote" : "No you can't vote";
+//	        System.out.println(canIVote);
+//}
+}
---
+++ LearningJavaATB11xProgram/src/ex_05_Ternary_Operator/Lab030_PositiveNum.java
@@ -0,0 +1,10 @@
+package ex_05_Ternary_Operator;
+
+public class Lab030_PositiveNum {
+	 public static void main(String[] args) {
+	        // result = condition ? expression1 : expression2;
+	        int number = 5;
+	        String resule = number > 0 ? "Positive" : "Negative";
+	        System.out.println(resule);
+	    }
+}
